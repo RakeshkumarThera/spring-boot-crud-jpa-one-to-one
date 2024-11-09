0ba41787386d79cb3028527d06e9d53bc90a7450
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-11-09 16:07:43 -0500

    DAO: findById, deleteById

--- src/main/java/com/rakesh/cruddemo/CruddemoApplication.java
+++ src/main/java/com/rakesh/cruddemo/CruddemoApplication.java
@@ -18,8 +18,27 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			createInstructor(appDAO);
+//			createInstructor(appDAO);
+//			findInstructor(appDAO);
+			deleteInstructor(appDAO);
 		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting the instructor id: " + theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Deleted!!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("Instructor: " + tempInstructor);
+		System.out.println("the associated instructor details only: " + tempInstructor.getInstructorDetail());
 	}
 
 	private void createInstructor(AppDAO appDAO) {
--- src/main/java/com/rakesh/cruddemo/dao/AppDAOImpl.java
+++ src/main/java/com/rakesh/cruddemo/dao/AppDAOImpl.java
@@ -23,4 +23,16 @@ public class AppDAOImpl implements AppDAO{
     public void save(Instructor theInstructor) {
         entityManager.persist(theInstructor);
     }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+        entityManager.remove(tempInstructor);
+    }
 }
