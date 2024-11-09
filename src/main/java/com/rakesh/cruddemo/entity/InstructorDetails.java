a14894cc821d0d9096f5c297d97f1bdbd5fbd6e8
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-11-08 19:37:34 -0500

    basic setup and created entity: instructor details

---
+++ src/main/java/com/rakesh/cruddemo/entity/InstructorDetails.java
@@ -0,0 +1,72 @@
+package com.rakesh.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetails {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toString() method
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannelName;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetails(){
+
+    }
+    public InstructorDetails(String youtubeChannelName, String hobby) {
+        this.youtubeChannelName = youtubeChannelName;
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannelName() {
+        return youtubeChannelName;
+    }
+
+    public void setYoutubeChannelName(String youtubeChannelName) {
+        this.youtubeChannelName = youtubeChannelName;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetails{" +
+                "id=" + id +
+                ", youtubeChannelName='" + youtubeChannelName + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
--- src/test/java/com/rakesh/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/rakesh/cruddemo/CruddemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rakesh.example.cruddemo;
+package com.rakesh.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
