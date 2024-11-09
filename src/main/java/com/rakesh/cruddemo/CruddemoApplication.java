583de041990be4f4a16d737aa7e6b604a94cfe34
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-11-09 15:41:37 -0500

    DAO: Save

--- src/main/java/com/rakesh/cruddemo/CruddemoApplication.java
+++ src/main/java/com/rakesh/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.rakesh.cruddemo;
 
+import com.rakesh.cruddemo.dao.AppDAO;
+import com.rakesh.cruddemo.entity.Instructor;
+import com.rakesh.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,33 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		//create the instructor
+		Instructor tempInstructor =
+				new Instructor("rakesh", "thera", "rakeshkumarthera@gmail.com");
+
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("https://www.youtube.com/@godswordministries.",
+						"Travelling");
+
+		//associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//save the instructors
+
+		//
+		//NOTE: this will save the details object
+		//because of CascadeType:ALL
+		//
+		System.out.println("Saving Instructors: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 }
---
+++ src/main/java/com/rakesh/cruddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.rakesh.cruddemo.dao;
+
+import com.rakesh.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+}
